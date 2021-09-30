d259edf1b7ee58dbccdce747b73927e5d513a543
dredhorse5 <Dima.titov18@gmail.com>
2021-09-30 04:10:04 +0300

    added load 's type a scene with button

--- Runtime/Scripts/Loading/LoadingSceneButton.cs
+++ Runtime/Scripts/Loading/LoadingSceneButton.cs
@@ -6,13 +6,20 @@ using UnityEngine.UI;
 
 public class LoadingSceneButton : MonoBehaviour
 {
+    public enum LoadTypes
+    {
+        Assync,
+        Linear
+    }
     public enum SceneTypes
     {
         LoadNewScene,
         ReloadThisScene
     }
-    public string SceneName = "";
+
+    public LoadTypes LoadType;
     public SceneTypes SceneType;
+    public string SceneName = "";
     
     
     private void Start()
@@ -24,12 +31,12 @@ public class LoadingSceneButton : MonoBehaviour
             {
                 case SceneTypes.LoadNewScene:
                     if(SceneName != null && SceneName != "")
-                        SceneLoader.LoadNewScene(SceneName);
+                        SceneLoader.LoadNewScene(SceneName, LoadType == LoadTypes.Assync);
                     else
                         Debug.LogError("No scene selected in the button : " + gameObject.name);
                     break;
                 case SceneTypes.ReloadThisScene:
-                    SceneLoader.ReloadScene();
+                    SceneLoader.ReloadScene(LoadType == LoadTypes.Assync);
                     break;
             }
         });
