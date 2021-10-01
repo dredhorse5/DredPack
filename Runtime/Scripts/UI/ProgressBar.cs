cd1f0665a363eab49c17b86ed7ae8b8d9ef5007f
dredhorse5 <Dima.titov18@gmail.com>
2021-10-01 21:53:08 +0300

    HotUpdate -  added moved icon in the ProgressBar

--- Runtime/Scripts/UI/ProgressBar.cs
+++ Runtime/Scripts/UI/ProgressBar.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,21 @@ using UnityEngine.UI;
 public class ProgressBar : MonoBehaviour
 {
     public Image FillBar;
+    public RectTransform MovedObject;
+
+    
+    
+    private RectTransform rectTransform;
+    private void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
 
     public void SetValue(float value)
     {
         FillBar.fillAmount = value;
+        
+        if(MovedObject && rectTransform)
+            MovedObject.anchoredPosition = new Vector2(rectTransform.rect.width * FillBar.fillAmount, 0);
     }
 }
