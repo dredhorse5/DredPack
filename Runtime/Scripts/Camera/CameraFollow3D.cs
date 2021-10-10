f6fe0ac674004ac6783d1346ef5c81f83145377d
dredhorse5 <Dima.titov18@gmail.com>
2021-10-10 22:27:29 +0300

    3d camerafollower and swiper

---
+++ Runtime/Scripts/Camera/CameraFollow3D.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow3D : MonoBehaviour
+{
+    public enum FollowTypes
+    {
+        LocatedAtTarget, // LT
+        FollowForTarget  // FT
+    }
+
+    public FollowTypes FollowType = FollowTypes.LocatedAtTarget;
+    
+    // Located at target (LT)
+    [Range(0,100)] public float LT_MoveSpeed;
+    public bool LT_Rotating = true;
+    [Range(0,100)] public float LT_RotateSpeed;
+
+
+    private Transform target;
+
+    private void Update()
+    {
+        if(!target)
+            return;
+        
+        switch (FollowType)
+        {
+            case FollowTypes.LocatedAtTarget:
+                LT_UpdatePosition();
+                break;
+            case FollowTypes.FollowForTarget:
+                break;
+        }
+    }
+
+    public void LT_UpdatePosition()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position, LT_MoveSpeed * Time.deltaTime / 2f );
+        if(LT_Rotating)
+            transform.rotation = Quaternion.Lerp(transform.rotation, target.rotation, LT_RotateSpeed* Time.deltaTime / 2f);
+    }
+
+    #region Target
+
+    public void SetTarget(Transform _target) => target = _target;
+    public void SetTarget(MonoBehaviour _target) => target = _target.transform;
+
+    public void SetCameraToTarget()
+    {
+        transform.position = target.position;
+        transform.rotation = target.rotation;
+    }
+
+    #endregion
+    
+
+}
---
+++ Runtime/Scripts/UI/Swiper.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class Swiper : MonoBehaviour, IDragHandler, IBeginDragHandler,IEndDragHandler
+{
+    public float DeathZone = 50f;
+
+    public UnityEvent<Vector2> SwipeEven = new UnityEvent<Vector2>();
+    public UnityEvent<Vector2> MoveDeltaEvent= new UnityEvent<Vector2>();
+
+    private Vector2 pointPressed;
+    private bool canEvent;
+    
+    public void OnDrag(PointerEventData eventData)
+    {
+        if(!canEvent)
+            return;
+        
+        Vector2 delta = (eventData.position - pointPressed);
+
+        if (delta.x > DeathZone)
+        {
+            SwipeEven?.Invoke(Vector2.right);
+            canEvent = false;
+        }
+        else if (delta.x < -DeathZone)
+        {
+            SwipeEven?.Invoke(Vector2.left);
+            canEvent = false;
+        }
+        else if (delta.y > DeathZone)
+        {
+            SwipeEven?.Invoke(Vector2.up);
+            canEvent = false;
+        }
+        else if (delta.y < -DeathZone)
+        {
+            SwipeEven?.Invoke(Vector2.down);
+            canEvent = false;
+        }
+        MoveDeltaEvent?.Invoke(eventData.delta);
+        
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        pointPressed = eventData.position;
+        canEvent = true;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canEvent = true;
+    }
+}
