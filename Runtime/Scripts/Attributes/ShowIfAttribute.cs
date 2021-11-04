7934de253ee71dd5faf990bdacaddb8d0e39ad22
dredhorse5 <Dima.titov18@gmail.com>
2021-11-04 16:05:33 +0300

    added ShowIfAttribute

---
+++ Runtime/Scripts/Attributes/ShowIfAttribute.cs
@@ -0,0 +1,54 @@
+using System;
+using UnityEngine;
+[AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+public class ShowIfAttribute : PropertyAttribute
+{
+    public ActionOnConditionFail Action {get;private set;}
+    public ConditionOperator Operator {get;private set;}
+    public string[] Conditions {get;private set;}
+
+    public ShowIfAttribute(ActionOnConditionFail action, ConditionOperator conditionOperator, params string[] conditions)
+    {
+        Action  = action;
+        Operator = conditionOperator;
+        Conditions = conditions;
+    }
+    public ShowIfAttribute(ConditionOperator conditionOperator, params string[] conditions)
+    {
+        Action  = ActionOnConditionFail.DontDraw;
+        Operator = conditionOperator;
+        Conditions = conditions;
+    }
+    public ShowIfAttribute(ActionOnConditionFail action,params string[] conditions)
+    {
+        Action  = action;
+        Operator = ConditionOperator.And;
+        Conditions = conditions;
+    }
+    public ShowIfAttribute(params string[] conditions)
+    {
+        Action  = ActionOnConditionFail.DontDraw;
+        Operator = ConditionOperator.And;
+        Conditions = conditions;
+    }
+    
+    /*
+     *      public bool AnyBool;
+     *      [ShowIf(nameof(AnyBool))]
+     *      public float AnyField;
+     */
+}
+public enum ActionOnConditionFail
+{
+    // If condition(s) are false, don't draw the field at all.
+    DontDraw,
+    // If condition(s) are false, just set the field as disabled.
+    JustDisable,
+}
+public enum ConditionOperator
+{
+    // A field is visible/enabled only if all conditions are true.
+    And,
+    // A field is visible/enabled if at least ONE condition is true.
+    Or,
+}
