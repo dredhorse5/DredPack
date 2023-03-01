a9a4619f8d092454a40f5f4396ddd6b064afab40
dredhorse5 <55126346+dredhorse5@users.noreply.github.com>
2023-03-01 22:59:33 +0300

    readOlnly - in namespace

--- Runtime/Scripts/Attributes/ReadOnly.cs
+++ Runtime/Scripts/Attributes/ReadOnly.cs
@@ -3,24 +3,27 @@
 using UnityEditor;
 #endif
 
-public class ReadOnlyAttribute : PropertyAttribute { }
-
-#if UNITY_EDITOR
-[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
-public class ReadOnly : PropertyDrawer
+namespace DredPack.Help
 {
-	// Necessary since some properties tend to collapse smaller than their content
+    public class ReadOnlyAttribute : PropertyAttribute { }
+
+    #if UNITY_EDITOR
+    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    public class ReadOnly : PropertyDrawer
+    {
+		// Necessary since some properties tend to collapse smaller than their content
 	public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
 	{
-		return EditorGUI.GetPropertyHeight(property, label, true);
+	    return EditorGUI.GetPropertyHeight(property, label, true);
 	}
 
 	// Draw a disabled property field
 	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
 	{
-		GUI.enabled = false; // Disable fields
-		EditorGUI.PropertyField(position, property, label, true);
-		GUI.enabled = true; // Enable fields
-	}
+            GUI.enabled = false; // Disable fields
+	    EditorGUI.PropertyField(position, property, label, true);
+            GUI.enabled = true; // Enable fields
+        }
+    }
+    #endif
 }
-#endif
