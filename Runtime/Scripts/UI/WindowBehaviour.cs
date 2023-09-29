d1577e1c8cc00e922f915615e7d6a08a5554c3eb
dredhorse5 <55126346+dredhorse5@users.noreply.github.com>
2023-09-29 17:02:03 +0300

    Update WindowBehaviour - added OnClosed and OnOpened virtual methods

--- Runtime/Scripts/UI/WindowBehaviour.cs
+++ Runtime/Scripts/UI/WindowBehaviour.cs
@@ -12,19 +12,25 @@ namespace DredPack.UI
         protected virtual void OnEnable()
         {
             Window.OpenEvent.AddListener(OnOpen);
+            Window.EndOpenEvent.AddListener(OnOpened);
             Window.CloseEvent.AddListener(OnClose);
+            Window.EndCloseEvent.AddListener(OnClosed);
             Window.SwitchEvent.AddListener(OnSwitch);
         }
 
         protected virtual void OnDisable()
         {
             Window.OpenEvent.RemoveListener(OnOpen);
+            Window.EndOpenEvent.RemoveListener(OnOpened);
             Window.CloseEvent.RemoveListener(OnClose);
+            Window.EndCloseEvent.RemoveListener(OnClosed);
             Window.SwitchEvent.RemoveListener(OnSwitch);
         }
 
         protected virtual void OnOpen() { }
+        protected virtual void OnOpened() { }
         protected virtual void OnClose() { }
+        protected virtual void OnClosed(){}
         protected virtual void OnSwitch(bool currentState) { }
 
         
