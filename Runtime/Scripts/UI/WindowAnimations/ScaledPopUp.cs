471067a54afae3b50a1f1046a28dc9ec45b910bc
dredhorse5 <Dima.titov18@gmail.com>
2023-11-25 00:19:54 +0300

    scaled pop up

--- Runtime/Scripts/UI/WindowAnimations/ScaledPopUp.cs
+++ Runtime/Scripts/UI/WindowAnimations/ScaledPopUp.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using DredPack.UI.WindowAnimations;
+using NaughtyAttributes;
 using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
@@ -16,26 +17,47 @@ namespace DredPack.UI.WindowAnimations
         public Transform ScaledObject;
         
         [Header("Alpha")]
+        [CurveRange(0, 0, 1, 1, EColor.Green)]
+        public AnimationCurve AlphaOpenCurve = new AnimationCurve(new []{new Keyframe(0,0), new Keyframe(.5f,1f)});
+        [CurveRange(0, 0, 1, 1, EColor.Red)]
+        public AnimationCurve AlphaCloseCurve = new AnimationCurve(new []{new Keyframe(0,.5f), new Keyframe(.5f,0)});
+        [Space]
         public AlphaTypes AlphaType = AlphaTypes.ByCanvasGroup;
-        public enum AlphaTypes { ByCanvasGroup, ByImage, Custom }
-        public AnimationCurve AlphaCurve = new AnimationCurve(new []{new Keyframe(0,0), new Keyframe(.5f,1f)});
+        public enum AlphaTypes { ByCanvasGroup, ByImage_Beta/*, Custom*/ }
+
+        [AllowNesting,ShowIf(nameof(AlphaType), AlphaTypes.ByImage_Beta)]
         public Image Image;
-        public Image[] Images;
-        public Image[] CanvasGroups;
-        
-        
+        /*public Image[] Images;
+        public CanvasGroup[] CanvasGroups;
+
+        private float[] imagesAplhas;
+        private float[] canvasGroupsAplhas;
+
+        public override void OnInit(Window owner)
+        {
+            imagesAplhas = new float[Images.Length];
+            for (int i = 0; i < Images.Length; i++)
+                if (Images[i])
+                    imagesAplhas[i] = Images[i].color.a;
+            
+            canvasGroupsAplhas = new float[CanvasGroups.Length];
+            for (var i = 0; i < CanvasGroups.Length; i++)
+                if (CanvasGroups[i])
+                    canvasGroupsAplhas[i] = CanvasGroups[i].alpha;
+        }*/
+
         public override IEnumerator UpdateOpen(AnimationParameters parameters)
         {
             base.UpdateOpen(parameters);
-            if (AlphaType == AlphaTypes.ByImage)
+            if (AlphaType == AlphaTypes.ByImage_Beta)
                 window.Components.CanvasGroup.alpha = 1;
             for (float i = 0; i < 1f; i += Time.deltaTime * Speed * parameters.CustomSpeed)
             {
                 ScaledObject.localScale = ScaleOpenCurve.Evaluate(i) * Vector3.one;
-                UpdateAlpha(i);
+                UpdateAlpha(AlphaOpenCurve.Evaluate(i));
                 yield return null;
             }
-            UpdateAlpha(1f);
+            UpdateAlpha(AlphaOpenCurve.Evaluate(1f));
         }
 
 
@@ -45,21 +67,27 @@ namespace DredPack.UI.WindowAnimations
             for (float i = 0; i < 1f; i += Time.deltaTime * Speed * parameters.CustomSpeed)
             {
                 ScaledObject.localScale = ScaleCloseCurve.Evaluate(i) * Vector3.one;
-                UpdateAlpha(1f - i);
+                UpdateAlpha(AlphaCloseCurve.Evaluate(i));
                 yield return null;
             }
 
-            if (AlphaType == AlphaTypes.ByImage)
+            if (AlphaType == AlphaTypes.ByImage_Beta)
                 window.Components.CanvasGroup.alpha = 0; 
-            UpdateAlpha(0);
+            UpdateAlpha(AlphaCloseCurve.Evaluate(1f));
         }
 
         private void UpdateAlpha(float value)
         {
             switch (AlphaType)
             {
-                case AlphaTypes.ByCanvasGroup: window.Components.CanvasGroup.alpha = AlphaCurve.Evaluate(value); break;
-                case AlphaTypes.ByImage: Image.color = new Color(Image.color.r,Image.color.g,Image.color.b, AlphaCurve.Evaluate(value)); break;
+                case AlphaTypes.ByCanvasGroup: window.Components.CanvasGroup.alpha = value; break;
+                case AlphaTypes.ByImage_Beta: Image.color = new Color(Image.color.r,Image.color.g,Image.color.b, value); break;
+                /*case AlphaTypes.Custom:
+                    foreach (var img in Images)
+                    {
+                        img.color = new Color(img.color.r, img.color.g, img.color.b, value);
+                    }
+                    break;*/
             }
         }
 
