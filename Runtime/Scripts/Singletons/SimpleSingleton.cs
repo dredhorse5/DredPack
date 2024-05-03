6f72cb715564055e8b6469896f88836edcd2a86d
dredhorse5 <55126346+dredhorse5@users.noreply.github.com>
2024-05-03 22:44:06 +0300

    Update SimpleSingleton.cs

--- Runtime/Scripts/Singletons/SimpleSingleton.cs
+++ Runtime/Scripts/Singletons/SimpleSingleton.cs
@@ -15,5 +15,12 @@ namespace DredPack
                 return m_Instance;
             }
         }
+        
+        protected virtual void Awake()
+        {
+            if (!Application.isPlaying)
+                return;
+            m_Instance = this;
+        }
     }
 }
