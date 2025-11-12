f95ee84e472e5cd88256e6efe5b78e1289e95e65
dredhorse5 <55126346+dredhorse5@users.noreply.github.com>
2025-11-12 14:19:16 +0300

    Fix formatting and add AllWindows list in WindowsManager

--- Runtime/Scripts/UI/WindowsManager.cs
+++ Runtime/Scripts/UI/WindowsManager.cs
@@ -1,12 +1,13 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using DredPack.UI;
 
 namespace DredPack.Runtime.Scripts.UI
 {
     public class WindowsManager : GeneralSingleton<WindowsManager>
     {
-        public static Dictionary<string, Window> RegisteredWindows { get; private set; } =
-            new Dictionary<string, Window>();
+        public static Dictionary<string, Window> RegisteredWindows { get; private set; } = new Dictionary<string, Window>();
+        public static List<Window> AllWindows { get; private set; } = new List<Window>();
+        
 
         public static bool TryGetWindow(string id, out Window window)
         {
@@ -22,11 +23,13 @@ namespace DredPack.Runtime.Scripts.UI
         {
             if(!string.IsNullOrEmpty(window.RegisterID))
                 RegisteredWindows.TryAdd(window.RegisterID, window);
+            AllWindows.Add(window);
         }
         public static void UnRegisterWindow(Window window)
         {
             if(!string.IsNullOrEmpty(window.RegisterID) && RegisteredWindows.ContainsKey(window.RegisterID))
                 RegisteredWindows.Remove(window.RegisterID);
+            AllWindows.Remove(window);
         }
     }
 }
